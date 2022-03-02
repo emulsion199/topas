20f122ce67c5e2717a5e4f8d740393f0266bde55
emulsion199 <wodnjs471@naver.com>
2022-03-02 22:54:15 +0900

    update finance info

--- server/server.py
+++ server/server.py
@@ -2,10 +2,34 @@ from flask import Flask,jsonify, request
 from flask_cors import CORS
 from pykrx import stock
 import json
-
+from pandas import DataFrame 
+import pandas as pd
 app=Flask(__name__)
 CORS(app)
-@app.route('/update_name',methods=['GET','POST'])
+
+
+@app.route('/price',methods=['GET','POST']) #특정 주가 불러오기
+def getPrice():
+    col=["시가","고가","저가","종가","거래량"]
+    day=[]
+    price={}
+    r=request.json
+    ticker=str(r['ticker'])
+    startday="20210202"
+    endday="20210302"
+    df = stock.get_market_ohlcv(startday, endday, ticker)
+    for i in range(len(df)):
+        day.append(str(df.index[i])[0:10])
+    for i in range(0,len(day)):
+        price[day[i]]={"시가":str(df.iloc[i][0]),"고가":str(df.iloc[i][1]),"저가":str(df.iloc[i][2]),"종가":str(df.iloc[i][3]),"거래량":str(df.iloc[i][4])}
+    print(price)
+    return jsonify(price)
+
+
+
+
+
+@app.route('/update_name',methods=['GET','POST']) #모든 주식 종목 불러오기
 def getAllName():
     name_json={}
     for ticker in stock.get_market_ticker_list():
--- src/Component/SearchComponent/SearchComponent.js
+++ src/Component/SearchComponent/SearchComponent.js
@@ -6,13 +6,35 @@ const SearchComponent=()=>
     const [searchInput,set_searchInput]=useState("")
     //axios.get("http://localhost:5000/update_name").then((response)=>{console.log(response.data)})
     const finance_name_json=require('../../json_data/finance_name.json')
+    const reverse_dict={}
     const finance_list=[]
+    const price_list=[[],[],[],[],[]]
+    const col={0:'시가',1:'고가',2:'저가',3:'종가',4:'거래량'}
+    const [price,setprice]=useState([])
     const ticker=Object.keys(finance_name_json)
     for (var i=0;i<ticker.length;i++)
     {
         finance_list.push(finance_name_json[ticker[i]])
+        reverse_dict[finance_name_json[ticker[i]]]=ticker[i]
     }
     finance_list.sort()
+
+    const getPrice=(ticker)=>
+    {
+        axios.post("http://localhost:5000/price",{'ticker':String(ticker)}).then((response)=>{
+        
+        const price_date=Object.keys(response.data)
+        for (var i=0;i<price_date.length;i++)
+        {
+            for (var j=0;j<5;j++)
+            {
+                price_list[j].push(response.data[price_date[i]][col[j]])
+            }
+        }
+        setprice(price_list)
+        console.log(price)
+    })
+    }
     return (
         <div>
             <input value={searchInput} onChange={(e)=>set_searchInput(e.target.value)}></input>
@@ -26,12 +48,17 @@ const SearchComponent=()=>
                 {finance_list.map((fname,key)=>{
                     if(fname.indexOf(searchInput)!=-1){
                     return(
-                        
-                        <FinanceCard key={fname} name={fname}></FinanceCard>
+                        <div onClick={()=>{getPrice(reverse_dict[fname])}}>
+                        <FinanceCard key={fname} name={fname} ></FinanceCard>
+                        </div>
                     )
                     }
                 })}
                 
+            </div>
+            <div>
+                {price.map((a,index)=><div>{col[index]+' '+a+' '}</div>)}
+
             </div>
 
         </div>
